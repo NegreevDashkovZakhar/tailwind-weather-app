2b201ad083a9b810a9407e06854181917e555660
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-01-24 22:15:42 +0600

    [+]Added weather description output

--- src/scripts/getWeather.js
+++ src/scripts/getWeather.js
@@ -2,25 +2,26 @@ const apiKey = '9aaeda92fc69595d8b72a88a346725cd';
 const apiAdress = 'https://api.openweathermap.org/data/2.5';
 
 function getWeather() {
-    console.log('called get weather');
     getTodayTemperature();
 }
 
 function getTodayTemperature() {
     let temperatureElement = document.getElementById('today-temperature');
     let cityElement = document.getElementById('city-name');
+    let descriptionElement = document.getElementById('weather-description');
     let cityName = cityElement.value;
-    console.log(`city name is ${cityName}`);
     let request = new XMLHttpRequest();
-    request.open('GET', `${apiAdress}/weather?q=${cityName}&appid=${apiKey}`);
+    request.open('GET', `${apiAdress}/weather?q=${cityName}&lang=ru&appid=${apiKey}`);
     request.onload = () => {
         let data = JSON.parse(request.response);
+        console.log('recieved data %j', data);
         //Converting temperature to celsius
         let currentTemperature = (data.main.temp - 273.3).toFixed(2);
         //Using innerHTML might be not safe though it is used to insert special symbol such as &deg;
         temperatureElement.textContent = `${currentTemperature} `;
         temperatureElement.innerHTML = temperatureElement.innerHTML + '&deg;C';
-        cityElement.textContent = cityName;
+        cityElement.value = data.name;
+        descriptionElement.textContent = data.weather[0].description;
     };
     request.send();
 }
