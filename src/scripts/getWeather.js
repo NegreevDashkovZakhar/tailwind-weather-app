5b9d338d05d35a9c9b691fcf3a219b26eae72f33
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-01-24 22:39:49 +0600

    [+]Added dynamic set for image

--- src/scripts/getWeather.js
+++ src/scripts/getWeather.js
@@ -1,6 +1,16 @@
 const apiKey = '9aaeda92fc69595d8b72a88a346725cd';
 const apiAdress = 'https://api.openweathermap.org/data/2.5';
 
+const imagePath = 'images/weather/';
+const weatherImagesMap = {
+    'Rain': 'rainy.png',
+    'Snow': 'rainy.png',
+    'Clouds': 'cloudy.png',
+    'Sun': 'sunny.png',
+    'Default': 'sunny-cloudy.png'
+}
+
+
 function getWeather() {
     getTodayTemperature();
 }
@@ -9,6 +19,7 @@ function getTodayTemperature() {
     let temperatureElement = document.getElementById('today-temperature');
     let cityElement = document.getElementById('city-name');
     let descriptionElement = document.getElementById('weather-description');
+    let imageElement = document.getElementById('weather-image');
     let cityName = cityElement.value;
     let request = new XMLHttpRequest();
     request.open('GET', `${apiAdress}/weather?q=${cityName}&lang=ru&units=metric&appid=${apiKey}`);
@@ -22,6 +33,11 @@ function getTodayTemperature() {
         temperatureElement.innerHTML = temperatureElement.innerHTML + '&deg;C';
         cityElement.value = data.name;
         descriptionElement.textContent = data.weather[0].description;
+        if (weatherImagesMap.hasOwnProperty(data.weather[0].main)) {
+            imageElement.src = imagePath+weatherImagesMap[data.weather[0].main];
+        } else {
+            imageElement.src = imagePath+weatherImagesMap.Default;
+        }
     };
     request.send();
 }
